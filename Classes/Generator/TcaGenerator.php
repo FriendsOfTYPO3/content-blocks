eca3bb07b1fe501c887cd51348d48b9fa117b1d0
Andre Kraus <info@andrekraus.com>
2022-11-14 20:17:19 +0100

    [FEATURE] start with TCA registration

--- Classes/Generator/TcaGenerator.php
+++ Classes/Generator/TcaGenerator.php
@@ -17,25 +17,40 @@ declare(strict_types=1);
 
 namespace TYPO3\CMS\ContentBlocks\Generator;
 
+use TYPO3\CMS\ContentBlocks\Domain\Repository\ContentBlockConfigurationRepository;
 use TYPO3\CMS\ContentBlocks\Enumeration\FieldType;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
 
 class TcaGenerator
 {
-    public function generateTca(array $yamlConfiguration): array 
+    /**
+     * Manages to set up the TCA for each ContentBlock
+     *
+     * $contentBlocksConfig as method param mainly for unti test purposes.
+     *
+     * @param array $contentBlocksConfig    configuration of all ContentBlocks
+     */
+    public function setTca(array $contentBlocksConfig = null): array
     {
-        $result = [];
-        foreach ($yamlConfiguration['fields'] as $field) {
-            $fieldType = FieldType::from($field['type']);
-            $config = $fieldType->getTca();
-            foreach ($field['properties'] as $key => $value) {
-                if ($key === 'trim' && $value === '1') {
-                    $config['eval'] = 'trim';
-                    continue;
-                }
-                $config[$key] = $value;
-            }
-            $result[]['config'] = $config;
+        if ($contentBlocksConfig === null) {
+            /** @param array */
+            $contentBlocksConfig = GeneralUtility::makeInstance(ContentBlockConfigurationRepository::class)->findAll();
         }
+        // result only for unit test purposes.
+        $result = [];
+
+        foreach ($contentBlocksConfig as $contentBlock) {
+            /***************
+             * Add Content Element
+             */
+            if (
+                !isset($GLOBALS['TCA']['tt_content']['types'][$contentBlock['CType']]) ||
+                !is_array($GLOBALS['TCA']['tt_content']['types'][$contentBlock['CType']])
+            ) {
+                $GLOBALS['TCA']['tt_content']['types'][$contentBlock['CType']] = [];
+            }
+        }
+        // return result only for unit test purposes.
         return $result;
     }
 }
---
+++ Configuration/TCA/Overrides/tt_content.php
@@ -0,0 +1,25 @@
+<?php
+
+use TYPO3\CMS\ContentBlocks\Generator\TcaGenerator;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+
+defined('TYPO3') or die();
+
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+(static function () {
+    GeneralUtility::makeInstance(
+        TcaGenerator::class
+    )->setTca();
+})();
