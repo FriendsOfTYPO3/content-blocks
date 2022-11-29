4ec3db0d4522b0de98b614faddd8c2a86b6a448b
Andre Kraus <info@andrekraus.com>
2022-11-29 23:26:09 +0100

    [FEATURE] ContentElementDefinition: add composerName, vendor, package, publicPath, privatePath

--- Classes/Definition/ContentElementDefinition.php
+++ Classes/Definition/ContentElementDefinition.php
@@ -17,22 +17,39 @@ declare(strict_types=1);
 
 namespace TYPO3\CMS\ContentBlocks\Definition;
 
+use TYPO3\CMS\ContentBlocks\Backend\Preview\PreviewRenderer;
+
 final class ContentElementDefinition extends TypeDefinition
 {
     private string $description = '';
     private string $contentElementIcon = '';
     private string $contentElementIconOverlay = '';
     private bool $saveAndClose = false;
+    private string $composerName = '';
+    private string $vendor = '';
+    private string $package = '';
+    private string $publicPath = '';
+    private string $privatePath = '';
 
     public static function createFromArray(array $array, string $table = 'tt_content'): ContentElementDefinition
     {
+
+        // 'vendor' => $vendor,
+        // 'package' => $package,
+        // 'publicPath' => $path . $configurationService->getContentBlocksPublicPath() . DIRECTORY_SEPARATOR,
+        // 'privatePath' => $path . $configurationService->getContentBlocksPublicPath() . DIRECTORY_SEPARATOR,
         $array['typeField'] = 'CType';
-        $self = parent::createFromArray($array, $table);
+        $self = self::fromArray($array, $table);
         return $self
             ->withDescription($array['description'] ?? '')
             ->withContentElementIcon($array['contentElementIcon'] ?? '')
             ->withContentElementIconOverlay($array['contentElementIconOverlay'] ?? '')
-            ->withSaveAndClose(!empty($array['saveAndClose']));
+            ->withSaveAndClose(!empty($array['saveAndClose']))
+            ->withComposerName($array['composerName'] ?? '')
+            ->withVendor($array['vendor'] ?? '')
+            ->withPackage($array['package'] ?? '')
+            ->withPublicPath($array['publicPath'] ?? '')
+            ->withPrivatePath($array['privatePath'] ?? '');
     }
 
     public function toArray(): array
@@ -43,6 +60,10 @@ final class ContentElementDefinition extends TypeDefinition
             'contentElementIcon' => $this->contentElementIcon,
             'contentElementIconOverlay' => $this->contentElementIconOverlay,
             'saveAndClose' => $this->saveAndClose,
+            'vendor' => $this->vendor,
+            'package' => $this->package,
+            'publicPath' => $this->publicPath,
+            'privatePath' => $this->privatePath,
         ];
         return $array;
     }
@@ -60,6 +81,31 @@ final class ContentElementDefinition extends TypeDefinition
     public function getContentElementIconOverlay(): string
     {
         return $this->contentElementIconOverlay;
+    }
+
+    public function getCType(): string
+    {
+        return 'cb_' . str_replace('/', '-', $this->composerName);
+    }
+
+    public function getVendor(): string
+    {
+        return $this->vendor;
+    }
+
+    public function getPackage(): string
+    {
+        return $this->package;
+    }
+
+    public function getPublicPath(): string
+    {
+        return $this->publicPath;
+    }
+
+    public function getPrivatePath(): string
+    {
+        return $this->privatePath;
     }
 
     public function hasSaveAndClose(): bool
@@ -92,6 +138,41 @@ final class ContentElementDefinition extends TypeDefinition
     {
         $clone = clone $this;
         $clone->saveAndClose = $saveAndClose;
+        return $clone;
+    }
+
+    public function withComposerName(string $composerName): self
+    {
+        $clone = clone $this;
+        $clone->composerName = $composerName;
+        return $clone;
+    }
+
+    public function withVendor(string $vendor): self
+    {
+        $clone = clone $this;
+        $clone->vendor = $vendor;
+        return $clone;
+    }
+
+    public function withPackage(string $package): self
+    {
+        $clone = clone $this;
+        $clone->package = $package;
+        return $clone;
+    }
+
+    public function withPublicPath(string $publicPath): self
+    {
+        $clone = clone $this;
+        $clone->publicPath = $publicPath;
+        return $clone;
+    }
+
+    public function withPrivatePath(string $privatePath): self
+    {
+        $clone = clone $this;
+        $clone->privatePath = $privatePath;
         return $clone;
     }
 }
