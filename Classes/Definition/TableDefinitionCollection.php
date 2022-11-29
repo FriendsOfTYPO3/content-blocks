885edd5bea2c84e519e16b27604ef7f513c6393a
Andre Kraus <info@andrekraus.com>
2022-11-29 23:28:29 +0100

    [FEATURE] add yaml process functionality to TableDefinitionCollection

--- Classes/Definition/TableDefinitionCollection.php
+++ Classes/Definition/TableDefinitionCollection.php
@@ -17,7 +17,11 @@ declare(strict_types=1);
 
 namespace TYPO3\CMS\ContentBlocks\Definition;
 
-final class TableDefinitionCollection implements \IteratorAggregate
+use TYPO3\CMS\ContentBlocks\Service\ConfigurationService;
+use TYPO3\CMS\Core\SingletonInterface;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+
+final class TableDefinitionCollection implements \IteratorAggregate, SingletonInterface
 {
     /**
      * @var TableDefinition[]
@@ -66,18 +70,139 @@ final class TableDefinitionCollection implements \IteratorAggregate
         }
     }
 
-    public static function createFromArray(array $tableDefinitionArray): TableDefinitionCollection
+    public static function createFromArray(array $contentBlocksList): TableDefinitionCollection
     {
-        $tableDefinitionCollection = new TableDefinitionCollection();
-        foreach ($tableDefinitionArray as $tableDefinition) {
-            $tableDefinitionCollection->addTable(
-                TableDefinition::createFromTableArray(
-                    $tableDefinition['table'],
-                    $tableDefinition['tableConfiguration']
-                )
-            );
+        $tableDefinitionCollection = new self();
+
+        /** @var ConfigurationService $configurationService */
+        $configurationService = GeneralUtility::makeInstance(ConfigurationService::class);
+
+        $tableDefinition = [];
+
+        // Since we need to sum up all lvl 0 ContentBlock fields to tt_content,
+        // we have to handle the tt_content table a bit different from collection tables.
+        foreach ($contentBlocksList as $contentBlock) {
+            // basic ContentBlock / CType data
+            $cType = str_replace('/', '-', $contentBlock['composerJson']['name']);
+            $collectionTablePrefix = $configurationService->getDatabaseCollectionTablePrefix() . $cType; // needed for unique collection table names later
+            $ttContentColumnPrefix = $configurationService->getDatabaseTtContentPrefix() . $cType; // needed for unique column names in tt_content later
+
+            // collect data for tt_content from each ContentBlock
+            if (isset($contentBlock['yaml']['fields']) && count($contentBlock['yaml']['fields']) > 0) {
+                foreach ($contentBlock['yaml']['fields'] as $ttContentField) {
+
+                    // unique tt_content column name
+                    $currentColumnName = $ttContentColumnPrefix . '_' . $ttContentField['identifier'];
+
+                    $ttContentField = $tableDefinitionCollection->processCollections(
+                        $ttContentField,
+                        'tt_content',
+                        $currentColumnName,
+                        $collectionTablePrefix
+                    );
+
+                    // add to tt_content fields
+                    $tableDefinition['fields'][$currentColumnName] = [
+                        'identifier' => $currentColumnName,
+                        'config' => $ttContentField,
+                    ];
+                }
+            }
+
+            // elements for TypeDefinition
+            [$vendor, $package] = explode('/', $contentBlock['composerJson']['name']);
+            $path = $configurationService->getBasePath() . $package . DIRECTORY_SEPARATOR;
+
+            $tableDefinition['elements'][] = [
+                'composerName' => $contentBlock['composerJson']['name'],
+                'identifier' => $contentBlock['composerJson']['name'],
+                'columns' => array_keys($tableDefinition['fields']),
+                'vendor' => $vendor,
+                'package' => $package,
+                'publicPath' => $path . $configurationService->getContentBlocksPublicPath() . DIRECTORY_SEPARATOR,
+                'privatePath' => $path . $configurationService->getContentBlocksPublicPath() . DIRECTORY_SEPARATOR,
+            ];
+
         }
+        // add tt_content definition
+        $tableDefinitionCollection->addTable(
+            TableDefinition::createFromTableArray(
+                'tt_content',
+                $tableDefinition,
+            )
+        );
         return $tableDefinitionCollection;
+    }
+
+    /**
+     * reduce redundant code by using this method.
+     */
+    public function processCollections(array $field, string $table, string $currentColumnName, string $collectionTablePrefix = ''): array
+    {
+        // take care of collections
+        if (
+            $field['type'] === 'Collection'
+            && isset($field['properties']['fields'])
+            && count($field['properties']['fields']) > 0
+        ) {
+            // unique collection table name
+            $collectionTableName = (($collectionTablePrefix !== '') ? $collectionTablePrefix . '_' : '') . $table . '_' . $field['identifier'];
+
+            // enrich infos for inline relations
+            // enrich infos for inline relations
+            $field['properties']['foreign_table'] = $collectionTableName; // The table name of the child records
+            $field['properties']['foreign_field'] = $currentColumnName; // the field of the child record pointing to the parent record. This defines where to store the uid of the parent record.
+            $field['properties']['foreign_table_field'] = $table; // the field of the child record pointing to the parent record. This defines where to store the table name of the parent record.
+            $field['properties']['foreign_match_fields'] = [
+                $table => $currentColumnName
+            ]; // Array of field-value pairs to both insert and match against when writing/reading IRRE relations.
+
+            // add collection table to collection
+            $this->createCollectionTables(
+                $collectionTableName,
+                $field['properties']['fields']
+            );
+
+        }
+        return $field;
+    }
+
+    /**
+     * Create tables recursivly.
+     *
+     * @param string $table     -> Current table to process
+     * @param array $fieldsList -> the list of fields / columns for this table
+     * @param string $collectionTablePrefix -> recursivly enhanced, but for lvl 1 its only "cb_"
+     */
+    public function createCollectionTables(
+        string $table,
+        array $fieldsList,
+        string $collectionTablePrefix = ''
+    ) {
+        $tableDefinition = [];
+
+        // collect data for tt_content from each ContentBlock
+        if (count($fieldsList) > 0) {
+            foreach ($fieldsList as $field) {
+                // add to field to table
+                $tableDefinition['fields'][$field['identifier']] = [
+                    'identifier' => $field['identifier'], // currentColumnName
+                    'config' => $this->processCollections(
+                        $field,
+                        $table,
+                        $field['identifier'], // currentColumnName
+                        $collectionTablePrefix
+                    ),
+                ];
+            }
+        }
+
+        $this->addTable(
+            TableDefinition::createFromTableArray(
+                $table,
+                $tableDefinition,
+            )
+        );
     }
 
     /**
