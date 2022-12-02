bf028c1a76387187289119be5ad119215a318d22
Andre Kraus <info@andrekraus.com>
2022-12-02 15:41:33 +0100

    [FEATURE][WIP] TcaCodeGenerator add collection table standard TCA

--- Classes/CodeGenerator/TcaCodeGenerator.php
+++ Classes/CodeGenerator/TcaCodeGenerator.php
@@ -18,6 +18,7 @@ declare(strict_types=1);
 namespace TYPO3\CMS\ContentBlocks\CodeGenerator;
 
 use TYPO3\CMS\Core\Configuration\ExtensionConfiguration;
+use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 
 class TcaCodeGenerator
@@ -55,5 +56,132 @@ class TcaCodeGenerator
         $columnNames = array_keys($columns);
         $showitemFields = implode(',', $columnNames);
         return $showitemFields . ', --div--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:tabs.access, --palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.visibility;visibility, --palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.access;access, --palette--;;hiddenLanguagePalette,';
+    }
+
+    public static function getCollectionTableStandardTca(array $columns, string $table, string $labelField): array
+    {
+
+        if (ExtensionManagementUtility::isLoaded('lang')) {
+            $generalLanguageFile = 'EXT:lang/Resources/Private/Language/locallang_general.xlf';
+        } else {
+            $generalLanguageFile = 'EXT:core/Resources/Private/Language/locallang_general.xlf';
+        }
+        return [
+            'ctrl' => [
+                'label' => $labelField,
+                // 'label_alt' => 'test_label_alt',
+                'sortby' => 'sorting',
+                'tstamp' => 'tstamp',
+                'crdate' => 'crdate',
+                'cruser_id' => 'cruser_id',
+                'title' => 'This is a TEST CTA',
+                'delete' => 'deleted',
+                'versioningWS' => true,
+                'origUid' => 't3_origuid',
+                'hideTable' => true,
+                'hideAtCopy' => true,
+                'prependAtCopy' => 'LLL:' . $generalLanguageFile . ':LGL.prependAtCopy',
+                'transOrigPointerField' => 'l10n_parent',
+                'transOrigDiffSourceField' => 'l10n_diffsource',
+                'languageField' => 'sys_language_uid',
+                'enablecolumns' => [
+                    'disabled' => 'hidden',
+                    'starttime' => 'starttime',
+                    'endtime' => 'endtime',
+                ],
+                'typeicon_classes' => [
+                    'default' => 'ext-content_blocks',
+                ],
+                'security' => [
+                    'ignorePageTypeRestriction' => true,
+                ],
+            ],
+            'interface' => [
+                'showRecordFieldList' => '
+                    hidden
+                ',
+            ],
+            'types' => [
+                '1' => [
+                    'showitem' => self::getCollectionTableStandardShowItems($columns),
+                ],
+            ],
+            'palettes' => [
+                '1' => [
+                    'showitem' => ''
+                ],
+            ],
+            'columns' => [
+                'hidden' => [
+                    'exclude' => true,
+                    'label' => 'LLL:' . $generalLanguageFile . ':LGL.hidden',
+                    'config' => [
+                        'type' => 'check',
+                        'items' => [
+                            '1' => [
+                                '0' => 'LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:hidden.I.0'
+                            ]
+                        ]
+                    ]
+                ],
+                'starttime' => [
+                    'exclude' => true,
+                    'label' => 'LLL:' . $generalLanguageFile . ':LGL.starttime',
+                    'config' => [
+                        'type' => 'input',
+                        'renderType' => 'inputDateTime',
+                        'eval' => 'datetime',
+                        'default' => 0
+                    ],
+                    'l10n_mode' => 'exclude',
+                    'l10n_display' => 'defaultAsReadonly'
+                ],
+                'endtime' => [
+                    'exclude' => true,
+                    'label' => 'LLL:' . $generalLanguageFile . ':LGL.endtime',
+                    'config' => [
+                        'type' => 'input',
+                        'renderType' => 'inputDateTime',
+                        'eval' => 'datetime',
+                        'default' => 0,
+                        'range' => [
+                            'upper' => mktime(0, 0, 0, 1, 1, 2038)
+                        ]
+                    ],
+                    'l10n_mode' => 'exclude',
+                    'l10n_display' => 'defaultAsReadonly'
+                ],
+                'sys_language_uid' => [
+                    'exclude' => 1,
+                    'label' => 'LLL:' . $generalLanguageFile . ':LGL.language',
+                    'config' => [
+                        'type' => 'language',
+                    ]
+                ],
+                'l10n_parent' => [
+                    'displayCond' => 'FIELD:sys_language_uid:>:0',
+                    'exclude' => 1,
+                    'label' => 'LLL:' . $generalLanguageFile . ':LGL.l18n_parent',
+                    'config' => [
+                        'type' => 'select',
+                        'renderType' => 'selectSingle',
+                        'items' => [
+                            [
+                                '',
+                                0
+                            ]
+                        ],
+                        'foreign_table' => $table,
+                        'foreign_table_where' => 'AND ' . $table . '.pid=###CURRENT_PID### AND ' . $table . '.sys_language_uid IN (-1,0)',
+                        'default' => 0
+                    ]
+                ],
+                'l10n_diffsource' => [
+                    'config' => [
+                        'type' => 'passthrough'
+                    ]
+                ],
+            ]
+        ];
     }
 }
