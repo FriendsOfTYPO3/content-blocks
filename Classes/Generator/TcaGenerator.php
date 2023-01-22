e583164a84428e308835857bf832d8f5e571da36
Nikita Hovratov <nikita.h@live.de>
2023-01-22 13:32:16 +0100

    Remove superfluous label for palette
    Change-Id: Ia5ea47c0c3d2e2a62b70fd9e5a43f832e0b93b60

--- Classes/Generator/TcaGenerator.php
+++ Classes/Generator/TcaGenerator.php
@@ -79,7 +79,7 @@ class TcaGenerator
             'header',
             implode(',', $columns),
             '--div--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:tabs.appearance',
-            '--palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.frames;frames',
+            '--palette--;;frames',
             '--palette--;;appearanceLinks',
             '--div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:language',
             '--palette--;;language',
