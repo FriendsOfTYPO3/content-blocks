2145930580e264d16df306e464555bdf5a6ea93a
Nikita Hovratov <nikita.h@live.de>
2023-09-26 09:27:08 +0200

    Allow TYPO3 12.4 in ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -8,10 +8,10 @@ $EM_CONF[$_EXTKEY] = [
     'author_email' => 'typo3cms@typo3.org',
     'author_company' => '',
     'state' => 'stable',
-    'version' => '13.0.0',
+    'version' => '12.4.0-13.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '13.0.0',
+            'typo3' => '12.4.0-13.0.0',
         ],
         'conflicts' => [],
         'suggests' => [],
