4a0815ab61721778902e6ff4c61b18ed005ca1e6
Nikita Hovratov <nikita.h@live.de>
2023-11-15 17:00:16 +0100

    [TASK] Switch to xliff version 1.2
    Fixes: #42

--- Classes/Builder/ContentBlockSkeletonBuilder.php
+++ Classes/Builder/ContentBlockSkeletonBuilder.php
@@ -58,62 +58,68 @@ class ContentBlockSkeletonBuilder
             $basePath . '/' . ContentBlockPathUtility::getContentBlockDefinitionFileName(),
             Yaml::dump($contentBlockConfiguration->getYamlConfig(), 10, 2)
         );
+
+        $utc = new \DateTimeZone('UTC');
+        $date = (new \DateTime())->setTimezone($utc)->format('c');
         if ($contentBlockConfiguration->getContentType() === ContentType::CONTENT_ELEMENT) {
             $languageContent = <<<HEREDOC
 <?xml version="1.0"?>
-<xliff version="1.0">
-	<file datatype="plaintext" original="messages" source-language="en" product-name="$name">
+<xliff version="1.2" xmlns="urn:oasis:names:tc:xliff:document:1.2">
+	<file datatype="plaintext" original="Labels.xlf" source-language="en" date="$date" product-name="$name">
 		<header/>
 		<body>
-			<trans-unit id="$vendor.$name.title" xml:space="preserve">
+			<trans-unit id="$vendor.$name.title" resname="$vendor.$name.title">
 				<source>Content Block: $name</source>
 			</trans-unit>
-			<trans-unit id="$vendor.$name.description" xml:space="preserve">
+			<trans-unit id="$vendor.$name.description" resname="$vendor.$name.description">
 				<source>This is your content block description</source>
 			</trans-unit>
-			<trans-unit id="header.label" xml:space="preserve">
+			<trans-unit id="header.label" resname="title.label">
 				<source>Custom header title</source>
 			</trans-unit>
 		</body>
 	</file>
 </xliff>
+
 HEREDOC;
         } elseif ($contentBlockConfiguration->getContentType() === ContentType::RECORD_TYPE) {
             $languageContent = <<<HEREDOC
 <?xml version="1.0"?>
-<xliff version="1.0">
-	<file datatype="plaintext" original="messages" source-language="en" product-name="$name">
+<xliff version="1.2" xmlns="urn:oasis:names:tc:xliff:document:1.2">
+	<file datatype="plaintext" original="Labels.xlf" source-language="en" date="$date" product-name="$name">
 		<header/>
 		<body>
-			<trans-unit id="$vendor.$name.title" xml:space="preserve">
+			<trans-unit id="$vendor.$name.title" resname="$vendor.$name.title">
 				<source>Content Block: $name</source>
 			</trans-unit>
-			<trans-unit id="$vendor.$name.description" xml:space="preserve">
+			<trans-unit id="$vendor.$name.description" resname="$vendor.$name.description">
 				<source>This is your content block description</source>
 			</trans-unit>
-			<trans-unit id="title.label" xml:space="preserve">
+			<trans-unit id="title.label" resname="title.label">
 				<source>Custom title</source>
 			</trans-unit>
 		</body>
 	</file>
 </xliff>
+
 HEREDOC;
         } else {
             $languageContent = <<<HEREDOC
 <?xml version="1.0"?>
-<xliff version="1.0">
-	<file datatype="plaintext" original="messages" source-language="en" product-name="$name">
+<xliff version="1.2" xmlns="urn:oasis:names:tc:xliff:document:1.2">
+	<file datatype="plaintext" original="Labels.xlf" source-language="en" date="$date" product-name="$name">
 		<header/>
 		<body>
-			<trans-unit id="$vendor.$name.title" xml:space="preserve">
+			<trans-unit id="$vendor.$name.title" resname="$vendor.$name.title">
 				<source>Content Block: $name</source>
 			</trans-unit>
-			<trans-unit id="$vendor.$name.description" xml:space="preserve">
+			<trans-unit id="$vendor.$name.description" resname="$vendor.$name.description">
 				<source>This is your content block description</source>
 			</trans-unit>
 		</body>
 	</file>
 </xliff>
+
 HEREDOC;
         }
 
