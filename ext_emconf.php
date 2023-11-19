b8331204fae576919e43f835e494e74bfa86e5a0
Nikita Hovratov <nikita.h@live.de>
2023-11-19 20:52:38 +0100

    Update ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -2,7 +2,7 @@
 
 $EM_CONF[$_EXTKEY] = [
     'title' => 'TYPO3 CMS Content Blocks',
-    'description' => 'TYPO3 CMS Content Blocks - TYPO3 Content Types Manager.',
+    'description' => 'TYPO3 CMS Content Blocks - Content Types API',
     'category' => 'be',
     'author' => 'TYPO3 Content Types Team',
     'author_email' => 'typo3cms@typo3.org',
