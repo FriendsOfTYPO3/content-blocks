474cae3a5360e08ad969df3ddab92c6fb771ce79
Philipp Kuhlmay <philipp.kuhlmay@wacon.de>
2024-01-12 13:38:50 +0100

    add test for UuidFieldConfiguration

---
+++ Tests/Unit/FieldTypes/UuidFieldConfigurationTest.php
@@ -0,0 +1,115 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+namespace TYPO3\CMS\ContentBlocks\Tests\Unit\FieldTypes;
+
+use TYPO3\CMS\ContentBlocks\FieldConfiguration\UuidFieldConfiguration;
+use TYPO3\TestingFramework\Core\Unit\UnitTestCase;
+
+final class UuidFieldConfigurationTest extends UnitTestCase
+{
+    public static function getTcaReturnsExpectedTcaDataProvider(): iterable
+    {
+        yield 'truthy values' => [
+            'config' => [
+                'label' => 'foo',
+                'description' => 'foo',
+                'displayCond' => [
+                    'foo' => 'bar',
+                ],
+                'l10n_display' => 'foo',
+                'l10n_mode' => 'foo',
+                'onChange' => 'foo',
+                'exclude' => true,
+                'size' => 30,
+                'version' => 4,
+                'enableCopyToClipboard' => false,
+            ],
+            'expectedTca' => [
+                'label' => 'foo',
+                'description' => 'foo',
+                'displayCond' => [
+                    'foo' => 'bar',
+                ],
+                'l10n_display' => 'foo',
+                'l10n_mode' => 'foo',
+                'onChange' => 'foo',
+                'exclude' => true,
+                'config' => [
+                    'type' => 'uuid',
+                    'size' => 30,
+                    'version' => 4,
+                    'enableCopyToClipboard' => false,
+                ],
+            ],
+        ];
+
+        yield 'falsy values' => [
+            'config' => [
+                'label' => '',
+                'description' => null,
+                'displayCond' => [],
+                'l10n_display' => '',
+                'l10n_mode' => '',
+                'onChange' => '',
+                'exclude' => false,
+                'non_available_field' => 'foo',
+                'max' => 0,
+                'min' => 0,
+                'size' => 0,
+                'version' => 0,
+                'enableCopyToClipboard' => false,
+            ],
+            'expectedTca' => [
+                'config' => [
+                    'type' => 'uuid',
+                    'enableCopyToClipboard' => false,
+                ],
+            ],
+        ];
+    }
+
+    /**
+     * @test
+     * @dataProvider getTcaReturnsExpectedTcaDataProvider
+     */
+    public function getTcaReturnsExpectedTca(array $config, array $expectedTca): void
+    {
+        $fieldConfiguration = UuidFieldConfiguration::createFromArray($config);
+
+        self::assertSame($expectedTca, $fieldConfiguration->getTca());
+    }
+
+    public static function getSqlReturnsExpectedSqlDefinitionDataProvider(): iterable
+    {
+        yield 'default varchar column' => [
+            'uniqueColumnName' => 'cb_example_myText',
+            'expectedSql' => '',
+        ];
+    }
+
+    /**
+     * @test
+     * @dataProvider getSqlReturnsExpectedSqlDefinitionDataProvider
+     */
+    public function getSqlReturnsExpectedSqlDefinition(string $uniqueColumnName, string $expectedSql): void
+    {
+        $inputFieldConfiguration = UuidFieldConfiguration::createFromArray([]);
+
+        self::assertSame($expectedSql, $inputFieldConfiguration->getSql($uniqueColumnName));
+    }
+}
