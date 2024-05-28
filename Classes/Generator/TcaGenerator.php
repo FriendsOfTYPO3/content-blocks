4988c0c376df6810de1809851a757b4f948b238f
Nikita Hovratov <nikita.h@live.de>
2024-05-28 15:15:56 +0200

    [TASK] Use continue statement

--- Classes/Generator/TcaGenerator.php
+++ Classes/Generator/TcaGenerator.php
@@ -585,7 +585,10 @@ class TcaGenerator
         if (in_array($tcaFieldType, $itemsFieldTypes, true)) {
             $items = $column['config']['items'] ?? [];
             foreach ($items as $index => $item) {
-                $labelPath = (string)($item['labelPath'] ?? '');
+                if (!isset($item['labelPath'])) {
+                    continue;
+                }
+                $labelPath = $item['labelPath'];
                 unset($column['config']['items'][$index]['labelPath']);
                 if (!$this->languageFileRegistry->isset($name, $labelPath)) {
                     continue;
