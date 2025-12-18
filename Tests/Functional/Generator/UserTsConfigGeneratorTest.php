17d288951b972b969cec2c11310cd7181ac41413
Nikita Hovratov <nikita.h@live.de>
2025-12-18 17:17:34 +0100

    [TASK] Register UserTsConfig via attribute and add tests

---
+++ Tests/Functional/Generator/UserTsConfigGeneratorTest.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+namespace TYPO3\CMS\ContentBlocks\Tests\Functional\Generator;
+
+use PHPUnit\Framework\Attributes\Test;
+use TYPO3\CMS\ContentBlocks\Generator\UserTsConfigGenerator;
+use TYPO3\CMS\Core\TypoScript\IncludeTree\Event\BeforeLoadedUserTsConfigEvent;
+use TYPO3\TestingFramework\Core\Functional\FunctionalTestCase;
+
+class UserTsConfigGeneratorTest extends FunctionalTestCase
+{
+    protected array $testExtensionsToLoad = [
+        'typo3conf/ext/content_blocks/Tests/Fixtures/Extensions/test_content_blocks_b',
+        'typo3conf/ext/content_blocks',
+    ];
+
+    #[Test]
+    public function userTsConfigIsGenerated(): void
+    {
+        $userTsConfigGenerator = $this->get(UserTsConfigGenerator::class);
+        $beforeLoadedUserTsConfigEvent = new BeforeLoadedUserTsConfigEvent();
+
+        $userTsConfigGenerator->__invoke($beforeLoadedUserTsConfigEvent);
+
+        $expected = ['options.pageTree.doktypesToShowInNewPageDragArea := addToList(942)'];
+        $result = $beforeLoadedUserTsConfigEvent->getTsConfig();
+        self::assertSame($expected, $result);
+    }
+}
