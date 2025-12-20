59a3085230ed00685c713f41dc77ef39e7bbfa20
Nikita Hovratov <nikita.h@live.de>
2025-12-20 14:22:50 +0100

    [TASK] Use Core UpgradeWizardInterface

--- Classes/Update/ContentBlockFolderStructureMigration.php
+++ Classes/Update/ContentBlockFolderStructureMigration.php
@@ -18,9 +18,9 @@ declare(strict_types=1);
 namespace TYPO3\CMS\ContentBlocks\Update;
 
 use Symfony\Component\Finder\Finder;
+use TYPO3\CMS\Core\Attribute\UpgradeWizard;
 use TYPO3\CMS\Core\Package\PackageManager;
-use TYPO3\CMS\Install\Attribute\UpgradeWizard;
-use TYPO3\CMS\Install\Updates\UpgradeWizardInterface;
+use TYPO3\CMS\Core\Upgrades\UpgradeWizardInterface;
 
 #[UpgradeWizard('contentBlocksFolderStructureMigration')]
 readonly class ContentBlockFolderStructureMigration implements UpgradeWizardInterface
