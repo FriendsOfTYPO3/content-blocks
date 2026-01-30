e84794e43eff2adbea979a3c9e8d822ce2af1c73
Nikita Hovratov <nikita.h@live.de>
2026-01-30 12:59:01 +0100

    [WIP] Move JsonSchema to root

--- Build/JsonSchema/json-schema-bundler.js
+++ Build/JsonSchema/json-schema-bundler.js
@@ -8,6 +8,6 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 let schema = await $RefParser.bundle(join(__dirname, 'SchemaSources/content-element.json'));
 let jsonString = JSON.stringify(schema, null, 2);
 
-const outputPath = join(__dirname, '../../Resources/Private/JsonSchema/content-element.schema.json');
+const outputPath = join(__dirname, '../../JsonSchema/content-element.schema.json');
 await mkdir(dirname(outputPath), { recursive: true });
 await writeFile(outputPath, jsonString);
