e0735405518ade5ab7f540fbb7c505af9626a87d
Nikita Hovratov <nikita.h@live.de>
2026-02-18 18:37:56 +0100

    [TASK] Add Schema for Field Type Basic

---
+++ Tests/Unit/JsonSchemaValidation/BasicFieldSchemaTest.php
@@ -0,0 +1,67 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+namespace TYPO3\CMS\ContentBlocks\Tests\Unit\JsonSchemaValidation;
+
+use PHPUnit\Framework\Attributes\DataProvider;
+use PHPUnit\Framework\Attributes\Test;
+use TYPO3\CMS\ContentBlocks\JsonSchemaValidation\JsonSchemaValidator;
+use TYPO3\TestingFramework\Core\Unit\UnitTestCase;
+
+final class BasicFieldSchemaTest extends UnitTestCase
+{
+    public static function basicFieldSchemaValidationWorksAsExpectedDataProvider(): iterable
+    {
+        yield 'Only type' => [
+            'data' => (object)[
+                'name' => 'json/schema-test',
+                'fields' => [
+                    (object)[
+                        'identifier' => 'TYPO3/Header',
+                        'type' => 'Basic',
+                    ],
+                ],
+            ],
+            'valid' => true,
+        ];
+
+        yield 'unknown property' => [
+            'data' => (object)[
+                'name' => 'json/schema-test',
+                'fields' => [
+                    (object)[
+                        'identifier' => 'TYPO3/Header',
+                        'type' => 'Basic',
+                        'unknown' => 'unknown',
+                    ],
+                ],
+            ],
+            'valid' => false,
+        ];
+    }
+
+    #[Test]
+    #[DataProvider('basicFieldSchemaValidationWorksAsExpectedDataProvider')]
+    public function categoryFieldSchemaValidationWorksAsExpected(object $data, bool $valid): void
+    {
+        $jsonSchemaValidator = new JsonSchemaValidator();
+
+        $validationResult = $jsonSchemaValidator->validateContentElement($data, 'http://typo3.org/content-element.json');
+
+        self::assertSame($valid, $validationResult);
+    }
+}
