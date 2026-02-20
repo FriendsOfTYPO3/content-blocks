83d721fb05f0e1b44192791cca96a4e4ab42ce5c
Nikita Hovratov <nikita.h@live.de>
2026-02-20 10:35:12 +0100

    [TASK] Add Schema for Radio

---
+++ Tests/Unit/JsonSchemaValidation/RadioFieldSchemaTest.php
@@ -0,0 +1,128 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+namespace TYPO3\CMS\ContentBlocks\Tests\Unit\JsonSchemaValidation;
+
+use PHPUnit\Framework\Attributes\DataProvider;
+use PHPUnit\Framework\Attributes\Test;
+use TYPO3\CMS\ContentBlocks\JsonSchemaValidation\JsonSchemaValidator;
+use TYPO3\TestingFramework\Core\Unit\UnitTestCase;
+
+final class RadioFieldSchemaTest extends UnitTestCase
+{
+    public static function radioFieldSchemaValidationWorksAsExpectedDataProvider(): iterable
+    {
+        yield 'Only type' => [
+            'data' => (object)[
+                'name' => 'json/schema-test',
+                'fields' => [
+                    (object)[
+                        'identifier' => 'radio',
+                        'type' => 'Radio',
+                    ],
+                ],
+            ],
+            'valid' => true,
+        ];
+
+        yield 'Valid properties' => [
+            'data' => (object)[
+                'name' => 'json/schema-test',
+                'fields' => [
+                    (object)[
+                        'identifier' => 'radio',
+                        'type' => 'Radio',
+                        'behaviour' => (object)[
+                            'allowLanguageSynchronization' => true,
+                        ],
+                        'default' => 'default_value',
+                        'fieldControl' => (object)['foo' => 'bar'],
+                        'fieldInformation' => (object)['foo' => 'bar'],
+                        'fieldWizard' => (object)[
+                            'defaultLanguageDifferences' => (object)['foo' => 'bar'],
+                            'localizationStateSelector' => (object)['foo' => 'bar'],
+                            'otherLanguageContent' => (object)['foo' => 'bar'],
+                        ],
+                        'items' => [
+                            (object)[
+                                'label' => 'Item 1',
+                                'value' => 'value1',
+                            ],
+                            (object)[
+                                'label' => 'Item 2',
+                                'value' => 2,
+                            ],
+                        ],
+                        'itemsProcessors' => (object)[
+                            '10' => (object)[
+                                'class' => 'My\\Class',
+                                'parameters' => (object)['foo' => 'bar'],
+                            ],
+                        ],
+                        'itemsProcFunc' => 'My\\Class->myMethod',
+                        'itemsProcConfig' => (object)['foo' => 'bar'],
+                        'readOnly' => false,
+                    ],
+                ],
+            ],
+            'valid' => true,
+        ];
+
+        yield 'unknown property' => [
+            'data' => (object)[
+                'name' => 'json/schema-test',
+                'fields' => [
+                    (object)[
+                        'identifier' => 'radio',
+                        'type' => 'Radio',
+                        'unknown' => 'unknown',
+                    ],
+                ],
+            ],
+            'valid' => false,
+        ];
+
+        yield 'items without required value' => [
+            'data' => (object)[
+                'name' => 'json/schema-test',
+                'fields' => [
+                    (object)[
+                        'identifier' => 'radio',
+                        'type' => 'Radio',
+                        'items' => [
+                            (object)[
+                                'label' => 'Label',
+                            ],
+                        ],
+                    ],
+                ],
+            ],
+            'valid' => false,
+        ];
+    }
+
+    #[Test]
+    #[DataProvider('radioFieldSchemaValidationWorksAsExpectedDataProvider')]
+    public function radioFieldSchemaValidationWorksAsExpected(object $data, bool $valid): void
+    {
+        $jsonSchemaValidator = new JsonSchemaValidator();
+
+        $validationResult = $jsonSchemaValidator->validateContentElement($data, 'http://typo3.org/content-element.json');
+
+        self::assertSame($valid, $validationResult);
+    }
+}
