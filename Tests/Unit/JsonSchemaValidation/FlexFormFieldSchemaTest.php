a341e27e4927e0588401cbebdc6c49b469491c37
Nikita Hovratov <nikita.h@live.de>
2026-02-27 08:54:13 +0100

    [TASK] Add Schema for simple FlexForm

---
+++ Tests/Unit/JsonSchemaValidation/FlexFormFieldSchemaTest.php
@@ -0,0 +1,100 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+namespace TYPO3\CMS\ContentBlocks\Tests\Unit\JsonSchemaValidation;
+
+use PHPUnit\Framework\Attributes\DataProvider;
+use PHPUnit\Framework\Attributes\Test;
+use TYPO3\CMS\ContentBlocks\JsonSchemaValidation\JsonSchemaValidator;
+use TYPO3\TestingFramework\Core\Unit\UnitTestCase;
+
+final class FlexFormFieldSchemaTest extends UnitTestCase
+{
+    public static function flexFormFieldSchemaValidationWorksAsExpectedDataProvider(): iterable
+    {
+        yield 'Only type' => [
+            'data' => (object)[
+                'name' => 'json/schema-test',
+                'fields' => [
+                    (object)[
+                        'identifier' => 'settings',
+                        'type' => 'FlexForm',
+                    ],
+                ],
+            ],
+            'valid' => true,
+        ];
+
+        yield 'Valid properties' => [
+            'data' => (object)[
+                'name' => 'json/schema-test',
+                'fields' => [
+                    (object)[
+                        'identifier' => 'settings',
+                        'type' => 'FlexForm',
+                        'searchable' => true,
+                        'fields' => [
+                            (object)[
+                                'identifier' => 'alignment',
+                                'type' => 'Select',
+                                'renderType' => 'selectSingle',
+                                'items' => [
+                                    (object)[
+                                        'label' => 'Left',
+                                        'value' => 'left',
+                                    ],
+                                ],
+                            ],
+                            (object)[
+                                'identifier' => 'title',
+                                'type' => 'Text',
+                                'label' => 'Title',
+                                'description' => 'A description',
+                            ],
+                        ],
+                    ],
+                ],
+            ],
+            'valid' => true,
+        ];
+
+        yield 'unknown property' => [
+            'data' => (object)[
+                'name' => 'json/schema-test',
+                'fields' => [
+                    (object)[
+                        'identifier' => 'settings',
+                        'type' => 'FlexForm',
+                        'unknown' => 'unknown',
+                    ],
+                ],
+            ],
+            'valid' => false,
+        ];
+    }
+
+    #[Test]
+    #[DataProvider('flexFormFieldSchemaValidationWorksAsExpectedDataProvider')]
+    public function flexFormFieldSchemaValidationWorksAsExpected(object $data, bool $valid): void
+    {
+        $jsonSchemaValidator = new JsonSchemaValidator();
+
+        $validationResult = $jsonSchemaValidator->validateContentElement($data, 'http://typo3.org/content-element.json');
+
+        self::assertSame($valid, $validationResult);
+    }
+}
