0a1320f835a62042d8b37f136d03c445a7d92c99
Nikita Hovratov <nikita.h@live.de>
2026-05-20 14:56:13 +0200

    [BUGFIX] Avoid toArray serialization in Record decorator
    As ContentBlockData falls back to Record Object, there is
    no need to pre-resolve the record. For Core elements without
    Content Block definition, the processed array stays now empty.
    This way lazy loading will actually take effect as well.
    
    Fixes: #519

--- Classes/DataProcessing/ContentBlockDataDecorator.php
+++ Classes/DataProcessing/ContentBlockDataDecorator.php
@@ -59,7 +59,6 @@ final class ContentBlockDataDecorator
         $contentTypeDefinition = $this->contentTypeResolver->resolve($resolvedRecord);
         $resolvedContentBlockDataRelation = new ResolvedContentBlockDataRelation();
         $resolvedContentBlockDataRelation->record = $resolvedRecord;
-        $resolvedContentBlockDataRelation->resolved = $resolvedRecord->toArray();
         if ($contentTypeDefinition === null) {
             return $this->buildFakeContentBlockDataObject($resolvedContentBlockDataRelation);
         }
@@ -288,7 +287,6 @@ final class ContentBlockDataDecorator
         $contentBlockRelation = new ResolvedContentBlockDataRelation();
         $foreignTable = $item->getMainType();
         $contentBlockRelation->record = $item;
-        $contentBlockRelation->resolved = $item->toArray();
         $hasTableDefinition = $this->tableDefinitionCollection->hasTable($foreignTable);
         $collectionTableDefinition = null;
         if ($hasTableDefinition) {
