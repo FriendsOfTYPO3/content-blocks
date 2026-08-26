acca46e7fab23c1e8ad5c52eeda2ae2e83827683
Matthias Vogel <m.vogel@andersundsehr.com>
2026-08-26 13:16:26 +0200

    [TASK] use `f:render.text` in generated html

--- ext_emconf.php
+++ ext_emconf.php
@@ -11,7 +11,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '1.6.3',
     'constraints' => [
         'depends' => [
-            'typo3' => '13.4.19-13.4.99',
+            'typo3' => '13.4.35-13.4.99',
         ],
         'conflicts' => [],
         'suggests' => [],
